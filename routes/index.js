75bbae687d8775d69d8fe4e3406c87b5e46da14f
Nasrul Hazim <nasrulhazim.m@gmail.com>
2016-10-15 14:41:09 +0800

    add simple login

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.render('auth/login');
+});
+
+router.post('/login', function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+  
+  console.log(username, password);
+
+  if(username === '' || password === '') {
+  	res.redirect('/');
+  } else {
+  	res.send('yeah!');
+  }
 });
 
 module.exports = router;
